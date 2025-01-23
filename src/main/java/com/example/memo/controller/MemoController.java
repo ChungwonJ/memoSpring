e7b60fef69f7d7a4de359b8c7f91225f27b000ee
chungwonJ <wjdcjddnjs1234@gmail.com>
2025-01-23 16:52:33 +0900

    메모장 조건 완료

---
+++ src/main/java/com/example/memo/controller/MemoController.java
@@ -0,0 +1,105 @@
+package com.example.memo.controller;
+
+import com.example.memo.dto.MemoRequestDto;
+import com.example.memo.dto.MemoResponseDto;
+import com.example.memo.entity.Memo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping("/memos")
+public class MemoController {
+    private final Map<Long, Memo> memoList = new HashMap<>();
+
+    @PostMapping
+    public ResponseEntity<MemoResponseDto> crateMemo(@RequestBody MemoRequestDto dto) {
+        // 식별자가 1씩 증가 하도록 만듦
+        Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
+
+        // 요청받은 데이터로 Memo 객체 생성
+        Memo memo = new Memo(memoId, dto.getTitle(), dto.getContents());
+
+        // Inmemory DB에 Memo 저장
+        memoList.put(memoId, memo);
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> findMemoById(@PathVariable Long id) {
+
+        // 식별자의 Memo가 없다면?
+        Memo memo = memoList.get(id);
+
+        // NPE 방지
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> updateMemo(
+            @PathVariable Long id,
+            @RequestBody MemoRequestDto memoRequestDto
+    ) {
+
+        Memo memo = memoList.get(id);
+
+        // NPE 방지
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        // 필수값 검증
+        if (memoRequestDto.getTitle() == null || memoRequestDto.getContents() == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        // memo 수정
+        memo.update(memoRequestDto);
+
+        // 응답
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<MemoResponseDto> updateTitle(
+            @PathVariable Long id,
+            @RequestBody MemoRequestDto requestDto
+    ) {
+        Memo memo = memoList.get(id);
+
+        // NPE 방지
+        if (memo == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        // 필수값 검증
+        if (requestDto.getTitle() == null || requestDto.getContents() != null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        memo.updateTitle(requestDto);
+
+        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMemo(@PathVariable Long id) {
+
+        // memoList의 Key값에 id를 포함하고 있다면
+        if (memoList.containsKey(id)) {
+            // key가 id인 value 삭제
+            memoList.remove(id);
+
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        // 포함하고 있지 않은 경우
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
